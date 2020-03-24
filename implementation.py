3f705a7ba717cee3d02b568388de7b1f3622fdc8
Russell Chiu <chiurj2002@gmail.com>
2020-03-24 12:50:50 -0400

    Fixed /0 error

--- implementation.py
+++ implementation.py
@@ -21,7 +21,7 @@ def createX(length, step):
     return arange(0, length, step)
 
 def peak(fr, amp, wid):
-    x = createX(1000, 0.05) # 0-1000 every .05
+    x = createX(1000, 0.05)+0.001 # 0-1000 every .05
     q = 2 * 3 ** 0.5 * (2 * fr * fr + fr * wid) * (4 * fr * wid + wid * wid) ** -1
     y = lambda t: amp * (1 + q ** 2 * (t / fr - fr / t) ** 2) ** -0.5
     signal = y(x)
@@ -34,13 +34,14 @@ def superimpose(freqs, amps, wids):
     y = y / max(y)
     return array([x, y])
 
-frequencies = data[:,0,:-1]
-amplitudes = data[:,1,:-1]
-bandwidths = data[:,2,:-1]
+data = data[:,:,:-1]
+frequencies = data[:,0]
+amplitudes = data[:,1]
+bandwidths = data[:,2]
     
 lowE = superimpose(frequencies[0], prediction(amplitudes[0].reshape([11,1])), bandwidths[0])
 plot(ifft(lowE[1]))
-axis([-1, 5, -1, 1])
+axis([-0.5, 10, -1, 1])
 
 
 
